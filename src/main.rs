878fc96dacce2580bd13b60bab49b3f9fac98480
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-12-03 16:30:11 -0600

    feat(core): add default header security rules

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[macro_use] extern crate rocket;
 
+use rocket::shield::{ExpectCt, NoSniff, Prefetch, Referrer, Shield, XssFilter};
+use rocket::time::Duration;
+
 #[get("/healthcheck")]
 fn index() -> &'static str {
     "Hello, world!"
@@ -7,5 +10,12 @@ fn index() -> &'static str {
 
 #[launch]
 fn rocket() -> _ {
+    let report_uri = uri!("https://report.meisaku.app");
+    let shield = Shield::default()
+        .enable(Referrer::NoReferrer)
+        .enable(Prefetch::Off)
+        .enable(ExpectCt::ReportAndEnforce(Duration::days(30), report_uri))
+        .enable(XssFilter::EnableBlock);
+
     rocket::build().mount("/", routes![index])
 }
