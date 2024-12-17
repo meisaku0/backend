1e25c6307fab7c88664195f7de45f230d6ad6269
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-12-17 11:35:59 -0600

    chore(core): format code

--- crates/user/src/application/commands/create_user.rs
+++ crates/user/src/application/commands/create_user.rs
@@ -1,9 +1,11 @@
 use rocket::serde::json::Json;
 use rocket_validation::Validated;
+use sea_orm_rocket::Connection;
+use config::database::pool::Db;
 use shared::responses::error::Error;
 use crate::presentation::dto::{CreateUserDTO, UserCreatedDTO};
 
-pub fn action(user: Validated<Json<CreateUserDTO>>) -> Result<Json<UserCreatedDTO>, Error> { 
+pub fn action(user: Validated<Json<CreateUserDTO>>, conn: Connection<'_, Db>) -> Result<Json<UserCreatedDTO>, Error> { 
     let user = user.into_inner();
     
     Ok(Json(UserCreatedDTO {
--- crates/user/src/infrastructure/http/controller.rs
+++ crates/user/src/infrastructure/http/controller.rs
@@ -1,7 +1,9 @@
+use rocket::post;
 use rocket::serde::json::Json;
-use rocket::{post};
 use rocket_okapi::openapi;
 use rocket_validation::Validated;
+use sea_orm_rocket::Connection;
+use config::database::pool::Db;
 use shared::responses::error::Error;
 
 use crate::presentation::dto::{CreateUserDTO, UserCreatedDTO};
@@ -10,8 +12,8 @@ use crate::presentation::dto::{CreateUserDTO, UserCreatedDTO};
 ///
 /// This endpoint is used to create a new user and return the user's
 /// information.
-#[openapi]
+#[openapi(ignore = "conn")]
 #[post("/", data = "<user>")]
-pub fn create(user: Validated<Json<CreateUserDTO>>) -> Result<Json<UserCreatedDTO>, Error> {
-    crate::application::commands::create_user::action(user)
+pub fn create(user: Validated<Json<CreateUserDTO>>, conn: Connection<'_, Db>) -> Result<Json<UserCreatedDTO>, Error> {
+    crate::application::commands::create_user::action(user, conn)
 }
