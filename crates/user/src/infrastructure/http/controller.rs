3d96995a62e71cc241af2bb80e585cb902959b4a
sawa-ko <56084970+sawa-ko@users.noreply.github.com>
2024-12-17 14:59:33 -0600

    fix(user): api docs for user creation controller

--- crates/user/src/infrastructure/http/controller.rs
+++ crates/user/src/infrastructure/http/controller.rs
@@ -1,18 +1,21 @@
+use config::database::pool::Db;
 use rocket::post;
 use rocket::serde::json::Json;
 use rocket_okapi::openapi;
 use rocket_validation::Validated;
 use sea_orm_rocket::Connection;
-use config::database::pool::Db;
 use shared::responses::error::Error;
+
 use crate::presentation::dto::create_user::{CreateUserDTO, UserCreatedDTO};
 
-/// # Create user
+/// # Create
 ///
 /// This endpoint is used to create a new user and return the user's
 /// information.
-#[openapi(ignore = "conn")]
+#[openapi(ignore = "conn", tag = "User")]
 #[post("/", data = "<user>")]
-pub async fn create(user: Validated<Json<CreateUserDTO>>, conn: Connection<'_, Db>) -> Result<Json<UserCreatedDTO>, Error> {
+pub async fn create(
+    user: Validated<Json<CreateUserDTO>>, conn: Connection<'_, Db>,
+) -> Result<Json<UserCreatedDTO>, Error> {
     crate::application::commands::create_user::action(user, conn).await
 }
